14b44aad41195460bfc0ce7c5caf8319f25b6d73
“Khunakorn Pattayakorn” <khunakornp.@gmail.com>
2023-11-22 10:13:40 +0700

    Created persistant files

--- project_manage.py
+++ project_manage.py
@@ -12,20 +12,6 @@ def initializing():
             print(data)
             db.insert(data)
 
-
-# here are things to do in this function:
-
-    # create an object to read all csv files that will serve as a persistent state for this program
-
-    # create all the corresponding tables for those csv files
-
-    # see the guide how many tables are needed
-
-    # add all these tables to the database
-
-
-# define a funcion called login
-
 def login():
     user = input("Enter username: ")
     password = input("Enter password: ")
@@ -35,49 +21,38 @@ def login():
                 return [users["ID"], users["role"]]
     return
 
-
-# define a function called exit
 def exit():
     for tables in db.database:
         keys = tables.table[0].keys()
-        myFile = open(f"Modified files/{tables.table_name}" + ".modified", 'w')
+        myFile = open(tables.table_name, 'w')
         writer = csv.DictWriter(myFile, fieldnames= keys)
         writer.writeheader()
         writer.writerows(tables.table)
         myFile.close()
-        myFile = open(f"Modified files/{tables.table_name}" + ".modified", 'r')
-        print("The content of the csv file is:")
+        myFile = open(tables.table_name, 'r')
+        print(f"The content of {tables.table_name} is:")
         print(myFile.read())
         myFile.close()
 
-# here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
-
-
-# make calls to the initializing and login functions defined above
-
 initializing()
-session = True
-while session:
-    val = login()
-    while val == None:
-        print("Username or password is invalid.")
-        val = login()
-    if val[1] == 'admin':
-        pass
-    elif val[1] == 'student':
-        pass
-    elif val[1] == 'member':
-        pass
-    elif val[1] == 'lead':
-        pass
-    elif val[1] == 'faculty':
-        pass
-    elif val[1] == 'advisor':
-        pass
+# session = True
+# while session:
+#     val = login()
+#     while val == None:
+#         print("Username or password is invalid.")
+#         val = login()
+#     if val[1] == 'admin':
+#         pass
+#     elif val[1] == 'student':
+#         pass
+#     elif val[1] == 'member':
+#         pass
+#     elif val[1] == 'lead':
+#         pass
+#     elif val[1] == 'faculty':
+#         pass
+#     elif val[1] == 'advisor':
+#         pass
 
 
 # once everyhthing is done, make a call to the exit function
